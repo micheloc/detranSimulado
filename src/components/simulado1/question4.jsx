dec47ee027ac4ef401e8de103de817b082a4efba
micheloc <48493079+micheloc@users.noreply.github.com>
2020-01-21 14:43:59 -0300

    Update Page

--- src/components/simulado1/question4.jsx
+++ src/components/simulado1/question4.jsx
@@ -1,45 +1,42 @@
 import React, { Component } from 'react';
-import { Card, CardBody, CardHeader, CardFooter, Button } from 'reactstrap';
 import BootstrapTable from 'react-bootstrap-table-next';
 
 const question = [
-    { id: '1', question: 'O máximo possivel do eixo da via, caso se trate de uma via arterial de circulação nos dois sentidos, para realizar uma conversão à direita.' },
-    { id: '2', question: 'Do bordo direito, tratando-se de uma via coletora de dois sentidos, para realizar uma conversão à esquerda.' },
-    { id: '3', question: 'O máximo possivel do bordo direito da pista para realizar uma conversão à direita em uma via coletora.' },
-    { id: '4', question: 'Do bordo esquerdo, tratando-se de uma via coletora de dois sentidos, para realizar uma conversão à esquerda.' }
+  { id: 'A', question: 'O máximo possivel do eixo da via, caso se trate de uma via arterial de circulação nos dois sentidos, para realizar uma conversão à direita.' },
+  { id: 'B', question: 'Do bordo direito, tratando-se de uma via coletora de dois sentidos, para realizar uma conversão à esquerda.' },
+  { id: 'C', question: 'O máximo possivel do bordo direito da pista para realizar uma conversão à direita em uma via coletora.' },
+  { id: 'D', question: 'Do bordo esquerdo, tratando-se de uma via coletora de dois sentidos, para realizar uma conversão à esquerda.' }
 ];
-
 
 const columns = [
-    { dataField: 'id', text: 'Product ID', hidden: true },
-    { dataField: 'question', text: 'Alternativas' },
+  { dataField: 'id',text: ' ',           headerStyle: () => { return { width: '10%', textAlign: 'center' } }},
+  { dataField: 'question', text: ' ',  headerStyle: () => { return { width: '90%', textAlign: 'center' } }},
 ];
 
-export class question1 extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+export class Question_4 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+    this._handleSelect = this._handleSelect.bind(this);
+  }
 
-        }
-        this._handleSelect = this._handleSelect.bind(this);
-    }
+  _handleSelect(row) {
+    this.props.alternativa(row.id);
+  }
 
-    _handleSelect(row) {
-        this.props.alternativa(row.id);
-    }
+  render() {
+    const selectRow = {
+      mode: 'radio', 
+      bgColor: '#bbb9b9',
+      clickToSelect: true, 
+      hideSelectColumn: true, 
+      onSelect: this._handleSelect
+    };
 
-    render() {
-        const selectRow = {
-            mode: 'radio',
-            clickToSelect: true,
-            onSelect: this._handleSelect
-        };
-        return (
-            <div>
-                <BootstrapTable keyField='id' data={question} columns={columns} selectRow={selectRow} />
-            </div>
-        );
-    }
+    return (
+      <div><BootstrapTable keyField='id' data={question} columns={columns} selectRow={selectRow} /></div>
+    );
+  }
 }
 
-export default question1;
+export default Question_4;
