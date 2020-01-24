48408426abb97ff3c855221634778bc1569fc70e
micheloc <48493079+micheloc@users.noreply.github.com>
2020-01-24 07:44:00 -0300

    Update Page

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
 
 export class home extends Component {
-    render() {
-        return (
-            <div>
-                <center><h1>Bem vindos ao Simuladoon</h1></center>
-                <center><h3>Neste site você tem total liberdade nas questões,</h3> </center>
-                <center><h3>para que possa ser utilizado como um material de estudo,</h3></center>
-                <center><h3>todas as alternativas foram tiradas de simulados utilizado em auto escolas do estado de Goiás.</h3></center>
-                <center><h3>Cada simulado são compostos por 30 alternativas, sendo elas de marcação.</h3></center>
-                <center><h3>Este simulado não contém tempo regressivo, o tempo dispõe do estudante para assimilar e decorar as alternativas.</h3></center>
-                <center><h2>Observação:</h2></center>
-                <center><h3>Ao final de cada simulado será gerado um gabarito</h3></center>
-                <center><h3>A cor vermelha corresponde as questões selecionadas, a cor azul corresponde as alternativas corretas</h3></center>
-                <center><h2>Tenham um bom proveito!!!</h2></center>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <center><h1>Bem vindos ao Simuladoon</h1></center>
+        <center><h3>Neste site você tem total liberdade nas questões,</h3> </center>
+        <center><h3>para que possa ser utilizado como um material de estudo,</h3></center>
+        <center><h3>todas as alternativas foram tiradas de simulados utilizado em auto escolas do estado de Goiás.</h3></center>
+        <center><h3>Cada simulado são compostos por 30 alternativas, sendo elas de marcação.</h3></center>
+        <center><h3>Este simulado não contém tempo regressivo, o tempo dispõe do estudante para assimilar e decorar as alternativas.</h3></center>
+        <center><h2>Observação:</h2></center>
+        <center><h3>Ao final de cada simulado será gerado um gabarito</h3></center>
+        <center><h3>A cor vermelha corresponde as questões selecionadas, a cor azul corresponde as alternativas corretas</h3></center>
+        <center><h2>Tenham um bom proveito!!!</h2></center>
+      </div>
+    );
+  }
 }
-
 export default home;
